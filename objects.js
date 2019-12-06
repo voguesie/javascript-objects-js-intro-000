36433e17dc5d2e4dc42f53640451f0581da6bc20
Michael Vogel <voguesie@alumni.stanford.edu>
2019-12-06 03:41:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,5 +2,5 @@ var playlist = {"Bon Iver": "Stacks"}
 // Function "updatePlaylist" accepts 3 parameters: the playlist (an object), an artist, and a song title.
 
 function updatePlaylist = (obj, artist, song) {
-  return Object.assign(obj, artist, song);
+  return Object.assign(obj, {artist: song});
 }
